0ff8385d30f78ea992ee7658e5a2547e77873a07
kobolddoido <kobolddoido@gmail.com>
2014-01-14 20:09:17 -0200

    todos os 3 scritpts numerados por ordem de execução

--- encontrar_marcas_cooloc.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'lucas'
-''' Encontra marcas de RBP em sequencias que se co-localizam com lncrnas'''
-arquivo_coolocalizados=open('saida_procurar_colocalizacao_lncrna.txt','r').read()
-for linha in arquivo_coolocalizados.split('\n'):
-    if len(linha)>0:
-        print linha
