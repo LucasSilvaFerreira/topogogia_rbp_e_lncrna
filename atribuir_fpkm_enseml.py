43b6829c1be237c7a14d3b002f0d37ab0a61cc43
Lucas Ferreira da Silva <lucassilva@fera>
2014-01-23 14:51:39 -0200

    Totalmente corrigido a assincronia do save nos processadores -hora de checar os scripts antigos para corrigir esse problema que provavelmente causou a perda de alguns dados

--- atribuir_fpkm_enseml.py
+++ atribuir_fpkm_enseml.py
@@ -3,17 +3,31 @@ from multiprocesso_classe import thread_processador
 class atribuindo_nomes(thread_processador):
     def funcao(self, inicio, fim):#teste
         import re
+        self.contador=0
         self.array_temporario = []
         self.arquivo_para_cruzar = open('final_TSS-Cufflinks-0.9.3-GENCODE-v3c-K562-rep1.gtf', 'r').read()
-        print 'esse metodo deve ser sobrescrito com os parametros presentes nesse arquivo dentro da classe'
+        #print 'esse metodo deve ser sobrescrito com os parametros presentes nesse arquivo dentro da classe'
         for array_linha in self.arquivo_array[int(inicio):int(fim)]:
-            ensb_query= array_linha.split('\t')[15]
-            for linha in self.arquivo_para_cruzar.split('\n'):
-                if len(linha)>0:
-                    gene_id = linha.split('gene_id')[1].split(';')[0]
-                    if re.search(ensb_query,gene_id):
-                        print re.sub('\"|\;','',linha.split('\t')[8].split('FPKM')[1])
-                        self.array_temporario.append(array_linha+'\t'+re.sub('\"|\;','',linha.split('\t')[8].split('FPKM')[1]))
-        for escrever in self.array_temporario: #isso deve ser executado apenas no final da funcao
-            self.saida.write(escrever + '\n')
-atribuindo_nomes('saida_ensembl.txt',4,'saida_fpkm_ensembl.txt')
+            try:
+                ensb_query= array_linha.split('\t')[15]
+                for linha in self.arquivo_para_cruzar.split('\n'):
+                    if len(linha)>0:
+                        gene_id = linha.split('transcript_id')[1].split(';')[0]
+                        if re.search(ensb_query,gene_id):
+                            #print ensb_query,gene_id
+                           # print re.sub('\"|\;','',linha.split('\t')[8].split('FPKM')[1])
+                            self.array_temporario.append(array_linha+'\t'+re.sub('\"|\;','',linha.split('\t')[8].split('FPKM')[1]))
+                            #print array_linha+'\t'+re.sub('\"|\;','',linha.split('\t')[8].split('FPKM')[1])
+                           #print len(self.array_temporario)
+                            self.contador+=1
+                            break
+            except:
+               # print array_linha
+                pass
+        print self.contador
+        #print self.array_temporario
+        self.set_saida(self.array_temporario)
+        #for escrever in self.array_temporario: #isso deve ser executado apenas no final da funcao
+        #    self.saida_unica.append(escrever + '\n')
+atribuindo_nomes('saida_ensembl.txt',70,'saida_fpkm_ensembl.txt')
+
--- multiprocesso_classe.py
+++ multiprocesso_classe.py
@@ -1,5 +1,6 @@
 __author__ = 'lucas'
 import multiprocessing
+from multiprocessing import Manager
 class thread_processador:
     from itertools import count
 
@@ -13,8 +14,9 @@ class thread_processador:
         self.numero_threads= n_process
         self.arquivo_escolhido= file
         self.saida_nome=saida_nome
-        self.saida_unica=[]
-
+        self.manager= Manager()
+        self.saida_unica=self.manager.list()
+        
         #abrindo o arquivo desejado
         arquivo = open(self.arquivo_escolhido,'r').read() #sempre mude para seu arquivo
         #salvando o arquivo desejado
@@ -27,27 +29,36 @@ class thread_processador:
             if len(arquivo_linha)>0:
                 self.arquivo_array.append(arquivo_linha)
         tamanho_threads= len(self.arquivo_array)
-        print tamanho_threads
+       # print tamanho_threads
         range_threads= len(self.arquivo_array)/self.numero_threads
         #
         for thread_numero in range (0,self.numero_threads):
-            print thread_numero*range_threads,thread_numero*range_threads+range_threads-1 #estou perdendo o ultimo valor da tabela
+           # print thread_numero*range_threads,thread_numero*range_threads+range_threads-1 #estou perdendo o ultimo valor da tabela
             inicio_valor=thread_numero*range_threads
             fim_valor=thread_numero*range_threads+range_threads-1
-            print inicio_valor,fim_valor
-            print "Thread "+ str(thread_numero) + "SUCCESSFULLY CREATED"
+           # print inicio_valor,fim_valor
+           # print "Thread "+ str(thread_numero) + "SUCCESSFULLY CREATED"
             p = multiprocessing.Process(target=self.funcao, args=(inicio_valor,fim_valor))
-            p.start()
+            #p.start()
             #p.join()
-            #self.jobs.append(p)
-
+            
+            self.jobs.append(p)
+            p.start()
+        for processos in self.jobs:
+            processos.join()
+        print 'final',len(self.saida_unica)
+        for i in range(len(self.saida_unica)):
+            for linha_i in self.saida_unica[i]:
+                self.saida.write(linha_i+'\n')
         #for salvando in self.saida_unica:
         #    self.saida.write(salvando+'\n')
         #while True:
         #    for
         #    print len(self.jobs)
-
-
+    def set_saida(self,array_para_entrar):
+        #print 'setando'
+        self.saida_unica.append(array_para_entrar)
+        #print 'teste',len(self.saida_unica)
     def funcao(self,inicio,fim):#teste
         self.array_temporario=[]
         '''sobrescreva esse metodos'''
@@ -57,3 +68,6 @@ class thread_processador:
             print array_linha
         for escrever in self.array_temporario: #isso deve ser executado apenas no final da funcao
             self.saida.write(escrever+'\n')
+
+    #def salvar(self):
+     #   print len(self.saida_unica),'<---saida unica'
